7e54e506f2ea9a4a855b3b7f4a799f156217b25f
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-09 21:06:47 +0100

    scripts: Build minter

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,7 @@ gzip -c target/wasm32-unknown-unknown/release/clearing.wasm >target/wasm32-unkno
 cargo build --locked --target wasm32-unknown-unknown --release -p registry
 
 gzip -c target/wasm32-unknown-unknown/release/registry.wasm >target/wasm32-unknown-unknown/release/registry.wasm.gz
+
+cargo build --locked --target wasm32-unknown-unknown --release -p minter
+
+gzip -c target/wasm32-unknown-unknown/release/minter.wasm >target/wasm32-unknown-unknown/release/minter.wasm.gz
