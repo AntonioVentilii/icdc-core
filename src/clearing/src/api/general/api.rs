335fac4a84da75db52684827ce160aa568c54527
Antonio Ventilii <antonioventilii@gmail.com>
2026-03-22 22:27:27 +0100

    feat: Method for USD decimals

--- src/clearing/src/api/general/api.rs
+++ src/clearing/src/api/general/api.rs
@@ -1,11 +1,18 @@
 use ic_cdk_macros::query;
-use shared::types::Series;
+use shared::{constants::USD_DECIMALS, types::Series};
 
-use crate::memory::SERIES;
+use crate::{guards::caller_is_not_anonymous, memory::SERIES};
 
 /// Returns a list of all derivative series currently cached in the clearing canister.
 #[query]
 #[must_use]
 pub fn list_series() -> Vec<Series> {
     SERIES.with(|s| s.borrow().values().cloned().collect())
+}
+
+/// Returns the official number of decimals for USD accounting.
+#[query(guard = "caller_is_not_anonymous")]
+#[must_use]
+pub fn get_usd_decimals() -> u8 {
+    USD_DECIMALS
 }
